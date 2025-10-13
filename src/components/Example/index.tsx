b4415dd7ca0c50942613a753baa1ee950916c500
Tien Dung NGUYEN <tiendungchs@gmail.com>
2025-10-13 17:47:43 +0200

    select input example

--- src/components/Example/index.tsx
+++ src/components/Example/index.tsx
@@ -15,7 +15,7 @@ export default function Example({ examples }: ExampleProps) {
           <Typography variant="h6" marginBottom={1}>Example {index + 1}</Typography>
           <Grid container spacing={2} marginBottom={2}>
             <Grid size={6}>
-              <ExampleInput input={example.input} />
+              <ExampleInput input={example.input} exampleIndex={index} />
             </Grid>
             <Grid size={6}>
               <ExampleOutput output={example.output} />
